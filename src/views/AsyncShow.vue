6e51c6486c8702024edf014e1a59ca3ef4ad9c03
zhupan <zerozhupan@163.com>
2021-07-26 20:20:33 +0800

    feat:suspense学习

---
+++ src/views/AsyncShow.vue
@@ -0,0 +1,26 @@
+<template>
+  <!-- <h2>{{ result }}</h2> -->
+  <h2>供suspense组件使用</h2>
+  <img :src="result && result.message" />
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import axios from "axios";
+export default defineComponent({
+  name: "AysncShow",
+  async setup() {
+    const rawData = await axios.get("https://dog.ceo/api/breeds/image/random");
+    return {
+      result: rawData.data,
+    };
+
+    // return new Promise((resolve) => {
+    //   setTimeout(() => {
+    //     return resolve({
+    //       result: "asyncShow",
+    //     });
+    //   }, 3000);
+    // });
+  },
+});
+</script>
--- src/views/Vue3Module.vue
+++ src/views/Vue3Module.vue
@@ -7,11 +7,29 @@
     <h3 v-if="loading">loading...</h3>
     <img v-if="loaded" :src="result.message" />
   </div>
+  <hr />
+  <p>{{ error_sus }}</p>
+  <suspense>
+    <template #default>
+      <aysnc-show />
+    </template>
+    <template #fallback>
+      <h2>loading!...</h2>
+    </template>
+  </suspense>
 </template>
 <script lang="ts">
-import { defineComponent, ref, onMounted, onUnmounted, watch } from "vue";
+import {
+  defineComponent,
+  ref,
+  onMounted,
+  onUnmounted,
+  watch,
+  onErrorCaptured,
+} from "vue";
 import useMousePosition from "@/hooks/useMouseposition";
 import useURLLoader from "@/hooks/useURLLoaders";
+import AysncShow from "./AsyncShow.vue";
 
 interface DogResult {
   message: string;
@@ -19,8 +37,14 @@ interface DogResult {
 }
 export default defineComponent({
   name: "Vue3Module",
-  components: {},
+  components: { AysncShow },
   setup() {
+    // 错误抓取
+    let error_sus = ref(null);
+    onErrorCaptured((e: any) => {
+      error_sus.value = e;
+      return true;
+    });
     const { x, y } = useMousePosition();
     const { result, loading, error, loaded } = useURLLoader<DogResult>(
       "https://dog.ceo/api/breeds/image/random"
@@ -34,6 +58,7 @@ export default defineComponent({
       result,
       loading,
       loaded,
+      error_sus,
     };
   },
 });
